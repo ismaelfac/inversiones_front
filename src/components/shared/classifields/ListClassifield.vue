08f0086293d05c7c3ffbc828247a2a4bfebd11fc
Ismael E. Lastre Alvarez <isamelfac@gmail.com>
2019-04-12 02:36:34 -0500

    Auth

---
+++ src/components/shared/classifields/ListClassifield.vue
@@ -0,0 +1,41 @@
+<template>
+    <v-flex xs12>
+            <v-card color="white">
+              <v-layout row>
+                <v-flex xs7>
+                  <v-card-title primary-title>
+                    <div>
+                      <div class="headline">Oficinas BuenaVista</div>
+                      <div>Local 112</div>
+                      <div>Ismael Lastre</div>
+                    </div>
+                  </v-card-title>
+                </v-flex>
+                <v-flex xs5>
+                  <v-img
+                    src="https://cdn.vuetifyjs.com/images/cards/halcyon.png"
+                    height="125px"
+                    class="mt-2"
+                    contain
+                  ></v-img>
+                </v-flex>
+              </v-layout>
+              <v-divider light></v-divider>
+              <v-card-actions class="pa-3">
+                <router-link to="/about" color="primary">Comentarios</router-link>
+                <v-spacer></v-spacer>
+                <v-icon>star_border</v-icon>
+                <v-icon>star_border</v-icon>
+                <v-icon>star_border</v-icon>
+                <v-icon>star_border</v-icon>
+                <v-icon>star_border</v-icon>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+</template>
+<script>
+export default {
+    
+}
+</script>
+
